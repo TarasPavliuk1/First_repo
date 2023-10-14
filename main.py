f02d625f71af55fa3b8d73106dae41dc9c5e988b
Taras_Pavliuk <lilu07081989@gmail.com>
2023-10-14 10:24:53 +0300

    dz1

--- main.py
+++ main.py
@@ -9,17 +9,7 @@ def handle_media(file_name: Path, target_folder: Path):
     file_name.replace(target_folder / normalize(file_name.name))
 
 
-# def handle_documents(file_name: Path, target_folder: Path):
-#     target_folder.mkdir(exist_ok=True, parents=True)
-#     file_name.replace(target_folder / normalize(file_name.name))
 
-# def handle_audio(file_name: Path, target_folder: Path):
-#     target_folder.mkdir(exist_ok=True, parents=True)
-#     file_name.replace(target_folder / normalize(file_name.name))
-
-# def handle_video(file_name: Path, target_folder: Path):
-#     target_folder.mkdir(exist_ok=True, parents=True)
-#     file_name.replace(target_folder / normalize(file_name.name))
 
 
 
@@ -34,17 +24,3 @@ def handle_archive(file_name: Path, target_folder: Path):
         return
     file_name.unlink()
 
-# бот дописав
-# if __name__ == '__main__':
-#     folder_process = sys.argv[1]
-#     file_parser.scan(Path(folder_process))
-
-#     # Обробка файлів згідно з file_parser
-#     for img in file_parser.JPEG_IMAGES:
-#         handle_media(img, Path('images'))
-
-#     for mp3 in file_parser.MP3_AUDIO:
-#         handle_media(mp3, Path('audio'))
-
-#     for zip_file in file_parser.ARCHIVES:
-#         handle_archive(zip_file, Path('archives'))
